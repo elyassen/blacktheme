731039eb62c8ff70b4f84471b95980023fa6fab6
yassen <yaseenseen9@gmail.com>
2024-04-17 20:35:13 +0530

    render

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,2 @@
-export const BASE_URL =
-  "https://blacktheme-backend-11a1jamyh-elyassens-projects.vercel.app";
-
+export const BASE_URL = "https://blacktheme-backend.onrender.com";
 // export const BASE_URL = "http://localhost:9090";
